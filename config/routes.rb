cb706053f871daac2b9fdea3f3b3a183474b658b
HoangThiTuanDung <hoang.thi.tuan.dung@framgia.com>
2015-04-08 16:44:38 +0700

    Signup login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
+  get 'signup' => 'users#new'
+  get 'login'  => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :users, only: [:new, :create, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
